a84a3376238065bf57227faca2c1d38b24980ae9
Clemens Kofler <clemens@railway.at>
2014-06-18 17:18:22 +0200

    Use method rather than instance variables

--- lib/flowster/workflowable.rb
+++ lib/flowster/workflowable.rb
@@ -1,23 +1,23 @@
 module Flowster
   module Workflowable
     def self.included(base)
-      base.send(:attr_accessor, :state)
+      base.send(:attr_accessor, :state, :workflow)
     end
 
     def with_workflow(name)
       begin
-        @workflow = Flowster.workflows[name]
+        self.workflow = Flowster.workflows[name]
         yield self
       ensure
-        @workflow = nil
+        self.workflow = nil
       end
     end
 
     # FIXME I'd rather not proxy everything to the workflow object all the time
     def method_missing(name, *args, &block)
-      return super unless @workflow.respond_to?(name)
+      return super unless workflow.respond_to?(name)
 
-      @workflow.send(name, *([self] + args), &block)
+      workflow.send(name, *([self] + args), &block)
     end
   end
 end
